4ae93629e22b4118fc5b4b11609876270909ed4b
qhdwight <qhdwight@gmail.com>
2024-09-08 18:39:06 -0700

    comments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Dawn Debian Package Generator
 
-1) Clone the desired dawn version in dawn/
+1) Clone the desired dawn version into dawn/
 2) Run `./build.sh`. The resulting debian file will be in build/
--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
+# We want a shared library webgpu_dawn.so as the output.
+# However we want all dependencies to be compiled in statically.
+# The combinatiion of setting BUILD_SHARED_LIBS to off in the cmake list and applying this patch ensures that.
 pushd dawn
 git apply ../build_shared.patch || true
 popd
